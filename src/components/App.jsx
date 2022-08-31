40025b3999109de98fc6ebbf6da54599aaabe0ba
Agami-Malikov <agami.malikov@gmail.com>
2022-08-31 17:52:30 +0300

    add start files

---
+++ src/components/App.jsx
@@ -0,0 +1,17 @@
+export const App = () => {
+  console.log('hello');
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 60,
+        color: '#010101',
+      }}
+    >
+      React homework template
+    </div>
+  );
+};
